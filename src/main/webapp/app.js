3417711e8a11ceab94c6a320c4da9c1bf33dfa9e
nastya-kostyukova <anastasya.kostyukova@gmail.com>
2016-04-25 01:25:41 +0300

    add routes, check password

--- src/main/webapp/app.js
+++ src/main/webapp/app.js
@@ -9,6 +9,15 @@ app.config(['$routeProvider', function($routeProvide){
         templateUrl: 'templates/home.html',
         controller: 'homeCtrl'
     })
+    .when('/profile', {
+        templateUrl: 'templates/profile.html'
+    })
+    .when('/order', {
+        templateUrl: 'templates/order.html'
+    })
+    .when('/accountsettings', {
+        templateUrl: 'templates/accset.html'
+    })
     .otherwise({
         redirectTo :'/'
     })
@@ -30,16 +39,28 @@ app.controller('homeCtrl', ['$scope', '$http', '$location', function($scope, $ht
     });
 }]);
 app.controller('registerController', ['$scope','$http', function($scope, $http) {
+    $scope.errorMessage="";
     $scope.register = function (name, email, password, passwordRepeat) {
-        console.log(email);
+        $scope.errorMessage="";
+        var pattern = /[\d|\w]{6,10}/;
+       /* if (password != passwordRepeat) {
+            console.log(password+' '+passwordRepeat);
+            $scope.errorMessage="Passwords don't match";
+
+        } else */if (!pattern.exec(password)){
+            console.log(password);
+            $scope.errorMessage="Password should only consist of letters and numbers, be at least 6 and no longer than 10 characters";
+        }
         /*$http.post("register", user).success(function(data) {
             console.log('REGISTRATION');
         });*/
     };
 }]);
-app.controller('loginController', ['$scope','$http', function($scope, $http) {
+
+app.controller('loginController', ['$scope','$http', '$location', function($scope, $http, $location) {
     $scope.login = function (email, password) {
         console.log(email+' '+password);
+        $location.path('/profile');
         /*$http.post("register", user).success(function(data) {
          console.log('REGISTRATION');
          });*/
