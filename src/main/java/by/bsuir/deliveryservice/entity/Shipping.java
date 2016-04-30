8cc754fed291f759be12c2505e111a8684f23694
mariateseiko <m30tess@gmail.com>
2016-05-01 01:36:22 +0200

    Updated shipping entity

--- src/main/java/by/bsuir/deliveryservice/entity/Shipping.java
+++ src/main/java/by/bsuir/deliveryservice/entity/Shipping.java
@@ -1,4 +1,32 @@
 package by.bsuir.deliveryservice.entity;
 
 public enum Shipping {
+    ;
+    private String name;
+    private Double pricePerKg;
+    private Double pricePerKm;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPricePerKg() {
+        return pricePerKg;
+    }
+
+    public void setPricePerKg(Double pricePerKg) {
+        this.pricePerKg = pricePerKg;
+    }
+
+    public Double getPricePerKm() {
+        return pricePerKm;
+    }
+
+    public void setPricePerKm(Double pricePerKm) {
+        this.pricePerKm = pricePerKm;
+    }
 }
