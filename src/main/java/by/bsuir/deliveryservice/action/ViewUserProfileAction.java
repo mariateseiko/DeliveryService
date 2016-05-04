37c77c07c626b0b8e5352501326b3da058db0573
mariateseiko <m30tess@gmail.com>
2016-05-04 21:48:49 +0200

    Added user profile view action

---
+++ src/main/java/by/bsuir/deliveryservice/action/ViewUserProfileAction.java
@@ -0,0 +1,46 @@
+package by.bsuir.deliveryservice.action;
+
+import by.bsuir.deliveryservice.entity.User;
+import by.bsuir.deliveryservice.service.ServiceException;
+import by.bsuir.deliveryservice.service.UserService;
+import by.bsuir.deliveryservice.service.impl.UserServiceImpl;
+import com.opensymphony.xwork2.Action;
+import org.apache.log4j.Logger;
+import org.apache.struts2.ServletActionContext;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+public class ViewUserProfileAction implements Action {
+    public static final Logger LOG =Logger.getLogger(LoginAction.class);
+    private User user;
+
+    private Long userId;
+    private static UserService userService = UserServiceImpl.getInstance();
+    private static final String USER = "user";
+
+    @Override
+    public String execute() throws Exception {
+        if (userId == null) {
+            HttpServletRequest request = ServletActionContext.getRequest();
+            HttpSession session = request.getSession();
+            User user = (User)session.getAttribute(USER);
+            userId = user.getId();
+        }
+        try {
+            user = userService.viewUser(userId);
+        } catch (ServiceException e) {
+            LOG.error(e.getStackTrace());
+            return ERROR;
+        }
+        return SUCCESS;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+}
