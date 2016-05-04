b23ef2399d3f4096fa2f554a36f0c75d23febd06
mariateseiko <m30tess@gmail.com>
2016-05-04 18:04:56 +0200

    Added view courier orders action

---
+++ src/main/java/by/bsuir/deliveryservice/action/ViewCourierOrdersActions.java
@@ -0,0 +1,40 @@
+package by.bsuir.deliveryservice.action;
+
+import by.bsuir.deliveryservice.entity.Order;
+import by.bsuir.deliveryservice.entity.User;
+import by.bsuir.deliveryservice.service.OrderService;
+import by.bsuir.deliveryservice.service.ServiceException;
+import by.bsuir.deliveryservice.service.impl.OrderServiceImpl;
+import com.opensymphony.xwork2.Action;
+import org.apache.struts2.ServletActionContext;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.List;
+
+public class ViewCourierOrdersActions implements Action {
+    private Long courierId;
+    private static final String USER = "user";
+    private List<Order> orders;
+    private static OrderService orderService = OrderServiceImpl.getInstance();
+
+    @Override
+    public String execute() throws Exception {
+        Long courierId;
+        HttpServletRequest request = ServletActionContext.getRequest();
+        HttpSession session = request.getSession();
+        User user = (User)session.getAttribute(USER);
+        courierId = user.getId();
+
+        try {
+            orders = orderService.viewCourierOrders(courierId);
+        } catch(ServiceException e) {
+            return ERROR;
+        }
+        return SUCCESS;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
+    }
+}
