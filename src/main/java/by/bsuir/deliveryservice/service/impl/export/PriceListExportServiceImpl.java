f70d6b9b355c7bb57a33b72d5d92528f4d44350c
Andrew Grivachevsky <me@amgsus.net>
2016-05-06 00:36:22 +0300

    Improve a XLS template for a price list + code refactoring

--- src/main/java/by/bsuir/deliveryservice/service/impl/export/PriceListExportServiceImpl.java
+++ src/main/java/by/bsuir/deliveryservice/service/impl/export/PriceListExportServiceImpl.java
@@ -18,9 +18,11 @@ import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.hssf.util.HSSFColor;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.Font;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.util.CellRangeAddress;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -28,7 +30,9 @@ import java.io.OutputStreamWriter;
 import java.nio.charset.Charset;
 import java.text.DateFormat;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public final class PriceListExportServiceImpl extends AbstractExportService
     implements PriceListExportService
@@ -55,25 +59,33 @@ public final class PriceListExportServiceImpl extends AbstractExportService
                 case PDF:
                     throw new ServiceException(format.toString() +
                             " not supported for this document");
-                case XLS:
-                    return impl_exportToXls(shippingDao.selectAllShippings());
-                case CSV:
-                    return impl_exportToCsv(shippingDao.selectAllShippings());
             }
 
-        } catch (Exception e) {
-            throw new ServiceException("failed to export PRICELIST", e);
-        }
+            File file = getTemporaryFile(PREFIX, format.toString());
 
-        return null;
+            List<Shipping> shippings = shippingDao.selectAllShippings();
+
+            switch (format) {
+                case XLS:
+                    impl_exportToXls(file, shippings);
+                    break;
+                case CSV:
+                    impl_exportToCsv(file, shippings);
+                    break;
+            }
+
+            return file;
+
+        } catch (Exception e) {
+            throw new ServiceException("failed to export a price list", e);
+        }
     }
 
     // ----------------------------------------------------------------------
 
-    protected File impl_exportToCsv(List<Shipping> items) throws Exception
+    public void impl_exportToCsv(File file, List<Shipping> items)
+            throws Exception
     {
-        File file = getTemporaryFile(PREFIX, DocFormat.CSV.toString());
-
         CSVFormat csvFormat = CSVFormat.DEFAULT.withDelimiter(';');
 
         try (OutputStreamWriter osw = new OutputStreamWriter(
@@ -88,7 +100,7 @@ public final class PriceListExportServiceImpl extends AbstractExportService
 
                 /* Print header */
 
-                csv.printRecord("Тип", "Цена/км", "Цена/кг");
+                csv.printRecord("Тип", "Стоимость/км", "Стоимость/кг");
 
                 /* Print record list */
 
@@ -106,15 +118,18 @@ public final class PriceListExportServiceImpl extends AbstractExportService
 
             }
         }
-
-        return file;
     }
 
     // ----------------------------------------------------------------------
 
-    protected File impl_exportToXls(List<Shipping> items) throws Exception
+    public void impl_exportToXls(File file, List<Shipping> items)
+            throws Exception
     {
-        File file = getTemporaryFile(PREFIX, DocFormat.XLS.toString());
+        final int C_NAME = 0;
+        final int C_PRICE_PER_KM = 1;
+        final int C_PRICE_PER_KG = 2;
+
+        final Map<String, CellStyle> cellStyles = new HashMap<>();
 
         Workbook wb = null;
         Cell c;
@@ -122,61 +137,106 @@ public final class PriceListExportServiceImpl extends AbstractExportService
 
         try {
 
+            wb = new HSSFWorkbook();
+
+            /* Init cell styles */
+
+            Font titleFont = wb.createFont();
+            titleFont.setBold(true);
+
+            Font tableHeaderFont = wb.createFont();
+            tableHeaderFont.setBold(true);
+
+            Font cellFont = wb.createFont();
+
+            CellStyle cs = wb.createCellStyle();
+
+            cs.setFont(titleFont);
+            cs.setAlignment(CellStyle.ALIGN_CENTER);
+            cs.setVerticalAlignment(CellStyle.ALIGN_FILL);
+
+            cellStyles.put("TITLE", cs);
+
+            cs = wb.createCellStyle();
+
+            cs.setAlignment(CellStyle.ALIGN_CENTER);
+            cs.setVerticalAlignment(CellStyle.ALIGN_FILL);
+            cs.setFillPattern(CellStyle.SOLID_FOREGROUND);
+            cs.setFillForegroundColor(HSSFColor.LIGHT_GREEN.index);
+            cs.setFont(tableHeaderFont);
+            cs.setBorderTop(CellStyle.BORDER_THIN);
+            cs.setBorderBottom(CellStyle.BORDER_THIN);
+            cs.setBorderLeft(CellStyle.BORDER_THIN);
+            cs.setBorderRight(CellStyle.BORDER_THIN);
+
+            cellStyles.put("TBL_HEADER", cs);
+
+            cs = wb.createCellStyle();
+
+            cs.setAlignment(CellStyle.ALIGN_CENTER);
+            cs.setVerticalAlignment(CellStyle.ALIGN_FILL);
+            cs.setFont(cellFont);
+            cs.setBorderTop(CellStyle.BORDER_THIN);
+            cs.setBorderBottom(CellStyle.BORDER_THIN);
+            cs.setBorderLeft(CellStyle.BORDER_THIN);
+            cs.setBorderRight(CellStyle.BORDER_THIN);
+
+            cellStyles.put("CELL", cs);
+
+            /* Template */
+
             DateFormat dateFormat = DateFormat.getDateInstance();
 
-            wb = new HSSFWorkbook();
             Sheet earningsSheet = wb.createSheet(String.format(
                     "Прайс-лист (%s)", dateFormat.format(new Date())));
 
             int rowIndex = 0;
+
+            row = earningsSheet.createRow(rowIndex);
+
+            c = row.createCell(C_NAME);
+            c.setCellValue("ПРАЙС-ЛИСТ");
+            c.setCellStyle(cellStyles.get("TITLE"));
+
+            earningsSheet.addMergedRegion(new CellRangeAddress(rowIndex,
+                    rowIndex, C_NAME, C_PRICE_PER_KG));
+
+            rowIndex++;
+
             row = earningsSheet.createRow(rowIndex++);
 
-            /* Header cell style */
-
-            CellStyle cshDelivery = wb.createCellStyle();
-            CellStyle cshPricePerKm = wb.createCellStyle();
-            CellStyle cshPricePerKg = wb.createCellStyle();
-
-            cshDelivery.setFillBackgroundColor(HSSFColor.LIGHT_BLUE.index);
-            cshDelivery.setAlignment(CellStyle.ALIGN_CENTER);
-
-            cshPricePerKm.setFillBackgroundColor(HSSFColor.AQUA.index);
-            cshPricePerKm.setAlignment(CellStyle.ALIGN_CENTER);
-
-            cshPricePerKg.setFillBackgroundColor(HSSFColor.AQUA.index);
-            cshPricePerKg.setAlignment(CellStyle.ALIGN_CENTER);
-
-            c = row.createCell(0);
+            c = row.createCell(C_NAME);
             c.setCellValue("Тип доставки");
-            c.setCellStyle(cshDelivery);
+            c.setCellStyle(cellStyles.get("TBL_HEADER"));
 
-            c = row.createCell(1);
-            c.setCellValue("Цена/км");
-            c.setCellStyle(cshPricePerKm);
+            c = row.createCell(C_PRICE_PER_KM);
+            c.setCellValue("Стоимость/км");
+            c.setCellStyle(cellStyles.get("TBL_HEADER"));
 
-            c = row.createCell(2);
-            c.setCellValue("Цена/кг");
-            c.setCellStyle(cshPricePerKg);
+            c = row.createCell(C_PRICE_PER_KG);
+            c.setCellValue("Стоимость/кг");
+            c.setCellStyle(cellStyles.get("TBL_HEADER"));
 
             for (Shipping s : items) {
 
                 row = earningsSheet.createRow(rowIndex++);
 
-                c = row.createCell(0);
+                c = row.createCell(C_NAME);
                 c.setCellValue(s.getName());
+                c.setCellStyle(cellStyles.get("CELL"));
 
-                c = row.createCell(1);
+                c = row.createCell(C_PRICE_PER_KM);
                 c.setCellValue(String.format("%.2f", s.getPricePerKm()));
-                c.getCellStyle().setAlignment(CellStyle.ALIGN_CENTER); // ?
+                c.setCellStyle(cellStyles.get("CELL"));
 
-                c = row.createCell(2);
+                c = row.createCell(C_PRICE_PER_KG);
                 c.setCellValue(String.format("%.2f", s.getPricePerKg()));
-                c.getCellStyle().setAlignment(CellStyle.ALIGN_CENTER); // ?
+                c.setCellStyle(cellStyles.get("CELL"));
             }
 
-            earningsSheet.autoSizeColumn(0);
-            earningsSheet.autoSizeColumn(1);
-            earningsSheet.autoSizeColumn(2);
+            earningsSheet.autoSizeColumn(C_NAME);
+            earningsSheet.autoSizeColumn(C_PRICE_PER_KM);
+            earningsSheet.autoSizeColumn(C_PRICE_PER_KG);
 
             wb.write(new FileOutputStream(file)); // Save to file
 
@@ -187,7 +247,5 @@ public final class PriceListExportServiceImpl extends AbstractExportService
             }
 
         }
-
-        return file;
     }
 }
