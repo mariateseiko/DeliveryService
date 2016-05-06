09556b041839841b0ac92071c02210eea155dc10
mariateseiko <m30tess@gmail.com>
2016-05-06 19:55:17 +0200

    Added view pricelist action

---
+++ src/main/java/by/bsuir/deliveryservice/action/ViewPricelistAction.java
@@ -0,0 +1,27 @@
+package by.bsuir.deliveryservice.action;
+
+import by.bsuir.deliveryservice.entity.Shipping;
+import by.bsuir.deliveryservice.service.ServiceException;
+import by.bsuir.deliveryservice.service.ShippingService;
+import by.bsuir.deliveryservice.service.impl.ShippingServiceImpl;
+import com.opensymphony.xwork2.Action;
+
+import java.util.List;
+
+public class ViewPricelistAction implements Action {
+    private List<Shipping> shippings;
+    private static ShippingService shippingService = ShippingServiceImpl.getInstance();
+    @Override
+    public String execute() throws Exception {
+        try {
+            shippings = shippingService.viewPricelist();
+            return SUCCESS;
+        } catch (ServiceException e) {
+            return ERROR;
+        }
+    }
+
+    public List<Shipping> getShippings() {
+        return shippings;
+    }
+}
