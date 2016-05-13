3d7153d6ad41527013a26e603e6216fec4980f66
Andrew Grivachevsky <me@amgsus.net>
2016-05-14 02:54:11 +0300

    Add officeService.js

---
+++ src/main/webapp/services/officeService.js
@@ -0,0 +1,32 @@
+'use strict'
+
+app.factory('officeService', ['$http', function ($http) {
+    return {
+
+        getPrimaryOffice: function ($scope, $rootScope) {
+            $http.get('viewPrimaryOffice').then(function (response) {
+                $scope.primaryOffice = response.data;
+                $rootScope.primaryOffice = response.data;
+            });
+        },
+
+        updatePrimaryOffice: function ($scope, $rootScope, $office) {
+
+            var data = {
+                name: $office.name,
+                credentials: $office.credentials
+            };
+
+            $http.post('updatePrimaryOffice', data).then(function (response) {
+                if (response.status == 200 && response.data) {
+                    $scope.successMessage = "The primary office has been" +
+                        " updated.";
+                } else {
+                    $scope.errorMessage = "Failed to update the primary office.";
+                }
+            });
+
+        }
+
+    }
+}]);
