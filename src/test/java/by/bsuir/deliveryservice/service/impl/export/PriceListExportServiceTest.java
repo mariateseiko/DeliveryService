6805888b4c56d529becc147f01cdd6a046692257
Andrew Grivachevsky <me@amgsus.net>
2016-05-14 12:07:48 +0300

    Add tests for PriceListExportServiceImpl

--- src/test/java/by/bsuir/deliveryservice/service/impl/export/PriceListExportServiceTest.java
+++ src/test/java/by/bsuir/deliveryservice/service/impl/export/PriceListExportServiceTest.java
@@ -16,11 +16,34 @@ public class PriceListExportServiceTest
                     PriceListExportServiceFactory.getService();
 
     @Test
-    public void exportToXlsTest() throws Exception
+    public void testExportToXls() throws Exception
     {
         File xlsFile = File.createTempFile("prices-", ".xls"); // Hardcoded
         System.out.println("File: " + xlsFile.getCanonicalPath());
 
+        List<Shipping> shippings = new ArrayList<>();
+
+        Shipping s = new Shipping();
+
+        s.setName("%NAME%");
+        s.setPricePerKg(1.00);
+        s.setPricePerKm(2.00);
+
+        Random random = new Random();
+        int count = 5 + random.nextInt(10);
+
+        for (int i = 1; i < count; i++) {
+            shippings.add(s);
+        }
+
+        serviceImpl.impl_exportToXls(xlsFile, shippings);
+
+        Assert.assertTrue(xlsFile.exists() && xlsFile.length() > 0);
+    }
+
+    @Test
+    public void testExportToCsv() throws Exception
+    {
         File csvFile = File.createTempFile("prices-", ".csv"); // Hardcoded
         System.out.println("File: " + csvFile.getCanonicalPath());
 
@@ -39,9 +62,34 @@ public class PriceListExportServiceTest
             shippings.add(s);
         }
 
-        serviceImpl.impl_exportToXls(xlsFile, shippings);
         serviceImpl.impl_exportToCsv(csvFile, shippings);
 
-        Assert.assertTrue(xlsFile.exists() && xlsFile.length() > 0);
+        Assert.assertTrue(csvFile.exists() && csvFile.length() > 0);
+    }
+
+    @Test
+    public void testExportToPdf() throws Exception
+    {
+        File pdfFile = File.createTempFile("prices-", ".pdf"); // Hardcoded
+        System.out.println("File: " + pdfFile.getCanonicalPath());
+
+        List<Shipping> shippings = new ArrayList<>();
+
+        Shipping s = new Shipping();
+
+        s.setName("%NAME%");
+        s.setPricePerKg(1.00);
+        s.setPricePerKm(2.00);
+
+        Random random = new Random();
+        int count = 5 + random.nextInt(10);
+
+        for (int i = 1; i < count; i++) {
+            shippings.add(s);
+        }
+
+        serviceImpl.impl_exportToPdf(pdfFile, shippings);
+
+        Assert.assertTrue(pdfFile.exists() && pdfFile.length() > 0);
     }
 }
